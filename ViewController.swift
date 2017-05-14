bc48e7f2c23a7caf2d67dba34dbd5669440ba01f
Jorge Manrubia <jorge.manrubia@gmail.com>
2017-05-14 11:51:32 +0200

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RaiseMan
+//
+//  Created by JORGE MANRUBIA DIEZ on 14/05/2017.
+//  Copyright © 2017 JORGE MANRUBIA DIEZ. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
